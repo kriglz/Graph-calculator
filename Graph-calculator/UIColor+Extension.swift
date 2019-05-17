01f33e1a1a3fc662b765c6632c0851ce3c0a5bf9
Kristina Gelzinyte <kristina.gelzinyte@gmail.com>
2019-05-17 20:52:06 +0300

    Colors

--- Graph-calculator/UIColor+Extension.swift
+++ Graph-calculator/UIColor+Extension.swift
@@ -12,4 +12,16 @@ extension UIColor {
     static var highlightColor: UIColor {
         return UIColor(red: 0.506, green: 0.635, blue: 0.659, alpha: 1)
     }
+    
+    static var keyColor: UIColor {
+        return UIColor(red: 0.38, green: 0.388, blue: 0.388, alpha: 1)
+    }
+    
+    static var alternativeKeyColor: UIColor {
+        return UIColor(red: 0.31, green: 0.424, blue: 0.443, alpha: 1)
+    }
+    
+    static var keyBorderColor: UIColor {
+        return UIColor(red: 0.588, green: 0.59, blue: 0.59, alpha: 1)
+    }
 }
