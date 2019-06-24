d41b7781b809b9124cf314bbb859fcf2f790eceb
Kristina Gelzinyte <kristina.gelzinyte@gmail.com>
2019-06-24 15:25:23 -0700

    Cleanup

--- Graph-calculator/Helpers/Collection View/FixedSpacingCollectionViewFlowLayout.swift
+++ Graph-calculator/Helpers/Collection View/FixedSpacingCollectionViewFlowLayout.swift
@@ -10,18 +10,18 @@ import UIKit
 
 class FixedSpacingCollectionViewFlowLayout: UICollectionViewFlowLayout {
     
-    override func layoutAttributesForItem(at indexPath: IndexPath) -> UICollectionViewLayoutAttributes? {
-        let currentItemAttributes = super.layoutAttributesForItem(at: indexPath)
-        let numberOfItemsPerLine = Int(floor(self.collectionViewContentSize.width / self.itemSize.width))
-        
-        if currentItemAttributes != nil, indexPath.item % numberOfItemsPerLine != 0 {
-            let cellIndexInLine = CGFloat(indexPath.item % numberOfItemsPerLine)
-            
-            var itemFrame = currentItemAttributes!.frame
-            itemFrame.origin.x = self.itemSize.width * cellIndexInLine + self.minimumInteritemSpacing * cellIndexInLine
-            currentItemAttributes?.frame = itemFrame
-        }
-        
-        return currentItemAttributes
-    }
+//    override func layoutAttributesForItem(at indexPath: IndexPath) -> UICollectionViewLayoutAttributes? {
+//        let currentItemAttributes = super.layoutAttributesForItem(at: indexPath)
+//        let numberOfItemsPerLine = Int(floor(self.collectionViewContentSize.width / self.itemSize.width))
+//        
+//        if currentItemAttributes != nil, indexPath.item % numberOfItemsPerLine != 0 {
+//            let cellIndexInLine = CGFloat(indexPath.item % numberOfItemsPerLine)
+//            
+//            var itemFrame = currentItemAttributes!.frame
+//            itemFrame.origin.x = self.itemSize.width * cellIndexInLine + self.minimumInteritemSpacing * cellIndexInLine
+//            currentItemAttributes?.frame = itemFrame
+//        }
+//        
+//        return currentItemAttributes
+//    }
 }
