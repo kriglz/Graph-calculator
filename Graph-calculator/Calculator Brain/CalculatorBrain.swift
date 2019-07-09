b0e03d069f12f946fd2dff0595a79cc6d0bdcebf
Kristina Gelzinyte <kristina.gelzinyte@gmail.com>
2019-07-09 16:38:45 -0700

    Clean up

--- Graph-calculator/Calculator Brain/CalculatorBrain.swift
+++ Graph-calculator/Calculator Brain/CalculatorBrain.swift
@@ -12,7 +12,6 @@ struct CalculatorMemory {
     var storage: Dictionary<String, Double>?
 }
 
-
 struct CalculatorBrain {
     
 //Description string made out of description array
--- Graph-calculator/Calculator.swift
+++ Graph-calculator/Calculator.swift
@@ -17,41 +17,157 @@ protocol CalculatorDelegate: class {
 class Calculator: NSObject {
     
     weak var delegate: CalculatorDelegate?
+
+    private var brain = CalculatorBrain()
+    private var memory = CalculatorMemory()
     
-    private let operationQueue = OperationQueue()
+    private var userIsInTheMiddleOfTyping = false
     
-    private var isInTheMiddleOfTyping = false
-    private var currentNumericOperandValue: String?
-    
-    private func containsInCurrentValue(_ value: String) -> Bool {
-        if let currentNumeric = self.currentNumericOperandValue {
-            return currentNumeric.contains(value)
+    private var displayValue: Double {
+        get {
+            return Double(self.displayText) ?? 0.0
+        } set {
+            self.displayText = String(newValue)
         }
-        
-        return false
+    }
+    
+    private var displayText: String = "" {
+        didSet {
+            self.delegate?.calculator(self, didUpdateLastOperation: self.displayText)
+        }
+    }
+    
+    private var descriptionDisplayText: String = "" {
+        didSet {
+            self.delegate?.calculator(self, didUpdateDescription: self.descriptionDisplayText)
+        }
+    }
+    
+    private var memoryDisplayText: String = "" {
+        didSet {
+            self.delegate?.calculator(self, didUpdateMemory: self.memoryDisplayText)
+        }
     }
     
     func setOperand(_ operand: KeyType) {
-        if operand.isNumber {
-            self.currentNumericOperandValue = (self.currentNumericOperandValue ?? "") + "\(operand.numericValue)"
-            self.delegate?.calculator(self, didUpdateLastOperation: self.currentNumericOperandValue ?? "")
+        guard let operandDescription = Keypad.keyList[operand]?.description else {
             return
         }
         
-        if operand == .comma, let description = Keypad.keyList[operand]?.description, !self.containsInCurrentValue(description) {
-            self.currentNumericOperandValue = (self.currentNumericOperandValue ?? "0") + description
-            self.delegate?.calculator(self, didUpdateLastOperation: self.currentNumericOperandValue ?? "")
-            return
-        } else if operand == .comma {
-            return
+        switch operand {
+        case .undo:
+            self.undoButton()
+        case .allClear:
+            self.allClearButton()
+        case .rand:
+            self.randomGenerationButton()
+        case .memoryIn:
+            self.setMemory()
+        case .memoryOut:
+            self.getMemory()
+            
+        default:
+            if operand.isNumber || operand == .comma {
+                self.addDigit(operandDescription)
+            } else {
+                self.addMathematicalSymbol(operandDescription)
+            }
+        }
+    }
+        
+    private func addDigit(_ digit: String) {
+        if self.userIsInTheMiddleOfTyping {
+            if !self.displayText.contains(".") || digit != "." {
+                let textCurrentlyDisplayed = self.displayText
+                self.displayText = textCurrentlyDisplayed + digit
+            }
+        } else {
+            self.displayText = digit
+            self.userIsInTheMiddleOfTyping = true
+        }
+    }
+    
+    private func undoButton() {
+        if self.userIsInTheMiddleOfTyping {
+            if !self.displayText.isEmpty {
+                self.displayText.removeLast()
+                if self.displayText.isEmpty {
+                    self.displayText = "0.0"
+                    self.userIsInTheMiddleOfTyping = false
+                }
+            }
+            if self.descriptionDisplayText.isEmpty {
+                self.descriptionDisplayText = " "
+            }
+        } else {
+            brain.undoPreviousOperation()
+            displayDescription()
+            
+            if let result = brain.evaluate(using: memory.storage).result {
+                displayValue = result
+            }
+            displayDescription()
+        }
+    }
+    
+    private func allClearButton() {
+        self.brain.clearAll()
+        self.memory.storage = nil
+        self.memoryDisplayText = " "
+        self.displayDescription()
+        self.userIsInTheMiddleOfTyping = false
+    }
+    
+    private func randomGenerationButton() {
+        let maxNumber = Double(UInt32.max)
+        let randomNumber = Double(arc4random())
+        self.displayValue = randomNumber / maxNumber
+        self.userIsInTheMiddleOfTyping = true
+    }
+    
+    private func setMemory() {
+        self.memory.storage = ["M": self.displayValue]
+        self.memoryDisplayText = "M → " + String(self.displayValue)
+        self.userIsInTheMiddleOfTyping = false
+        
+        if !self.brain.description.isEmpty {
+            self.displayText = String(self.brain.evaluate(using: self.memory.storage).result!)
+        }
+    }
+    
+    private func getMemory() {
+        self.brain.setOperand(variable: "M")
+        self.displayValue = memory.storage?["M"] ?? 0
+    }
+    
+    private func addMathematicalSymbol(_ symbol: String) {
+        if self.userIsInTheMiddleOfTyping {
+            self.brain.setOperand(self.displayValue)
+            self.userIsInTheMiddleOfTyping = false
         }
         
-        if let stringValue = self.currentNumericOperandValue, let value = Double(stringValue) {
-            self.operationQueue.append(numeric: value)
-            self.currentNumericOperandValue = nil
+        self.brain.setOperand(variable: symbol)
+        
+        if let result = self.brain.evaluate(using: self.memory.storage).result {
+            self.displayValue = result
         }
         
-        self.operationQueue.append(operand)
-        self.delegate?.calculator(self, didUpdateDescription: self.operationQueue.description)
+        self.displayDescription()
+    }
+    
+    func displayDescription() {
+        if self.brain.evaluate().isPending {
+            self.descriptionDisplayText = self.brain.description + "..."
+//            self.changeGraphButtonStatusToNotReady()
+        } else {
+            if !self.brain.description.isEmpty {
+                self.descriptionDisplayText = self.brain.description + "="
+//                self.changeGraphButtonStatusToReady()
+            } else {
+                self.displayValue = 0
+                self.descriptionDisplayText = "0"
+//                self.changeGraphButtonStatusToNotReady()
+            }
+        }
     }
 }
