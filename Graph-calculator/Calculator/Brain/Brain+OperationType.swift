65af33ad769e7c485b60c538d9991c2ee985b723
Kristina Gelzinyte <kristina.gelzinyte@gmail.com>
2019-07-19 17:19:55 -0700

    Operation type improvement

--- Graph-calculator/Calculator/Brain/Brain+OperationType.swift
+++ Graph-calculator/Calculator/Brain/Brain+OperationType.swift
@@ -10,12 +10,10 @@ import Foundation
 
 extension CalculatorBrain {
     enum OperationType {
-        case numeric(Double)
         case constant(Double)
         case unary((Double) -> Double)
         case binary((Double, Double) -> Double)
         case equals
-        case other
         
         var description: String {
             switch self {
@@ -27,11 +25,7 @@ extension CalculatorBrain {
                 return "binaryOperation"
             case .equals:
                 return "equals"
-            default:
-                break
             }
-            
-            return "error"
         }
     }
 }
