e83d103821e3aa319e3dc97a1ab504e07e017cef
Kristina Gelzinyte <kristina.gelzinyte@gmail.com>
2019-07-22 16:00:03 -0700

    Fixed some parsing errors

--- Graph-calculator/Calculator/Brain/CalculatorBrain.swift
+++ Graph-calculator/Calculator/Brain/CalculatorBrain.swift
@@ -242,72 +242,101 @@ struct CalculatorBrain {
             var displayArray = [String]()
             var partialArray = [String]()
             var repetetiveNumber = 2
-            var lastOperationType = ""
             
+            var lastOperationType = ""
+            var lastOperationIndex = 0
+
             for element in descriptionArray {
                 partialArray.append(element)
                 
                 if let value = Double(element) {
                     displayArray.append(value.roundedToIntIfNeededString)
+                    lastOperationIndex = displayArray.index(before: displayArray.endIndex)
                  
                 } else if element == "M" || element == "x" {
                     displayArray.append(element)
-                    
+                    lastOperationIndex = displayArray.index(before: displayArray.endIndex)
+
                 } else if element == "x²" {
                     if lastOperationType == "equals" {
+                        lastOperationIndex = displayArray.startIndex
                         displayArray.insert("(", at: displayArray.startIndex)
-                        
+
                     } else if lastOperationType == "unaryOperation" {
-                        displayArray.insert("(", at: displayArray.index(before: displayArray.endIndex - repetetiveNumber))
+                        displayArray.insert("(", at: lastOperationIndex)
                         repetetiveNumber += 2
                     
                     } else {
-                        displayArray.insert("(", at: displayArray.index(before: displayArray.endIndex))
+                        let index = displayArray.index(before: displayArray.endIndex)
+                        lastOperationIndex = index
+                        displayArray.insert("(", at: index)
                     }
                     
                     displayArray.append(")" + "²")
                     
                 } else if element == "±" {
                     if performOperations(partialArray).result! < 0 {
-                        if lastOperationType == "equals" || lastOperationType == "unaryOperation" {
-                            displayArray.insert("-" + "(", at: descriptionArray.startIndex)
+                        if lastOperationType == "equals", displayArray.count > 1 {
+                            displayArray.insert("-(", at: 0)
                             displayArray.append(")")
+                            
+                        } else if lastOperationIndex != 0 && lastOperationType == "unaryOperation" {
+                            displayArray.insert("(-", at: lastOperationIndex)
+                            displayArray.append(")")
+                            
                         } else if lastOperationType == "binaryOperation" {
-                            displayArray.insert("(" + "-", at: displayArray.index(before: displayArray.endIndex))
+                            let index = displayArray.index(before: displayArray.endIndex)
+                            lastOperationIndex = index
+                            displayArray.insert("(" + "-", at: index)
                             displayArray.append(")")
+                            
+                        } else if lastOperationIndex == 0 {
+                            displayArray.insert("-", at: lastOperationIndex)
+                            
                         } else {
-                            displayArray.insert("-", at: displayArray.index(before: displayArray.endIndex))
+                            displayArray.insert("(-", at: lastOperationIndex)
+                            displayArray.append(")")
                         }
                         
                     } else if performOperations(partialArray).result! > 0 {
                         if lastOperationType == "equals" || lastOperationType == "unaryOperation" {
-                            displayArray.insert("-" + "(", at: descriptionArray.startIndex)
+                            lastOperationIndex = displayArray.startIndex
+                            displayArray.insert("-" + "(", at: displayArray.startIndex)
                             displayArray.append(")")
-                        }
-                        else {
-                            displayArray.insert("-,", at: displayArray.index(before: displayArray.endIndex))
+                        
+                        } else {
+                            let index = displayArray.index(before: displayArray.endIndex)
+                            lastOperationIndex = index
+                            displayArray.insert("-,", at: index)
                         }
                     }
                 
                 } else if let newOperationName = operationType(for: element), newOperationName != "equals" {
                     if (newOperationName == "binaryOperation" && lastOperationType != "equals") || newOperationName == "constant" {
                         displayArray.append(element)
+                        lastOperationIndex = displayArray.index(before: displayArray.endIndex)
                     } else {
                         if (newOperationName == "binaryOperation" && lastOperationType == "equals") {
+                            lastOperationIndex = displayArray.startIndex
                             displayArray.insert("(", at: displayArray.startIndex)
                             displayArray.append(")" + element)
                             
                         } else if lastOperationType == "equals" {
+                            lastOperationIndex = displayArray.startIndex
                             displayArray.insert(element + "(", at: displayArray.startIndex)
                             displayArray.append(")")
                             
                         } else {
                             if lastOperationType == "unaryOperation" && newOperationName == "unaryOperation" {
-                                displayArray.insert(element + "(", at: displayArray.index(before: displayArray.endIndex - repetetiveNumber))
+//                                let index = displayArray.index(before: displayArray.endIndex - repetetiveNumber)
+//                                lastOperationIndex = index
+                                displayArray.insert(element + "(", at: lastOperationIndex)
                                 repetetiveNumber += 2
                                 
                             } else {
-                                displayArray.insert(element + "(", at: displayArray.index(before: displayArray.endIndex))
+                                let index = displayArray.index(before: displayArray.endIndex)
+                                lastOperationIndex = index
+                                displayArray.insert(element + "(", at: index)
                             }
                             displayArray.append(")")
                         }
